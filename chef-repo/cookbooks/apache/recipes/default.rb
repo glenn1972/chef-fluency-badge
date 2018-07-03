2ed1113998ca799463a9a3ed6a731235d7f796ec
glenn1972 <user@glenndavies3.mylabserver.com>
2018-07-03 14:07:04 +0000

    added the postgresql cookook

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -8,9 +8,9 @@
 #
 #install apache package
 
-if node ['platform_family'] == "rhel"
+if node['platform_family'] == "rhel"
 	package = "httpd"
-elsif node ['platform_family'] == "debian"
+elsif node['platform_family'] == "debian"
 	package = "apache2"
 end
 
